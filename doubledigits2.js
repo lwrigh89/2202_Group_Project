320afdf8028cb529facc724ae68dabfbfd6d053b
pikakight <tmmarcus73@gmail.com>
2021-10-07 14:30:54 -0400

    here are the two different interpertations I have about the project

---
+++ doubledigits2.js
@@ -0,0 +1,42 @@
+
+let digits = [];
+
+function printSingles(){
+
+}
+        
+function printTens(){
+        
+}
+        
+function printTwinties(){
+        
+}
+        
+function printThirties(){
+        
+}
+        
+function printFourties(){
+        
+}
+        
+function printFifties(){
+        
+}
+        
+function printSixties(){
+        
+}
+        
+function printSeventies(){
+        
+}
+
+function printEighties(){
+        
+}
+        
+function printNinties(){
+        
+}
