65479a87742c1271976f7d612c81175ffde8f8d6
PikaKight <tmmarcus73@gmail.com>
2021-10-08 22:29:20 -0400

    Did 80s and fixed the 5 digit

--- doubledigits2.js
+++ doubledigits2.js
@@ -1,5 +1,15 @@
 
-let digits = [];
+let zero = [" _ ","| |","|_|"];
+let one = ["   ","|  ","|  "];
+let two = [" _ "," _|","|_ "];
+let three = [" _ "," _|"," _|"];
+let four = ["   ", "|_|", "  |"];
+let five = [" _ ", "|_ ", " _|"];
+let six = [" _ ", "|_ ", "|_|"];
+let seven = [" _ ", "  |", "  |"];
+let eight = [" _ ", "|_|", "|_|"];
+let nine = [" _ ", "|_|", "  |"];
+let digits = [zero, one, two, three, four, five, six, seven, eight, nine];
 
 function printSingles(){
 
@@ -34,9 +44,24 @@ function printSeventies(){
 }
 
 function printEighties(){
+    let eighties = "";
+    let firstDigit = digits[8];
+    for (i=0; i < digits.length; i++){
         
+        let secondDigit = digits[i];
+        
+        for (j = 0; j < 3; j++){
+            eighties += firstDigit[j] + secondDigit[j];
+            eighties += "\n"; 
+        }
+   }     
+   console.log(eighties);
 }
         
 function printNinties(){
         
+}
+
+if (!module.parent){
+    printEighties();
 }
