c10c8b5ce26923c084428c96c52367133c85c2e3
PikaKight <tmmarcus73@gmail.com>
2021-10-08 22:44:33 -0400

    fixed some stuff and did 80s and 90s

--- doubledigit.js
+++ doubledigit.js
@@ -1,45 +1,123 @@
-let digits = [];
+let zero = [" _ ","| |","|_|"];
+let one = ["   ","|  ","|  "];
+let two = [" _ "," _|","|_ "];
+let three = [" _ "," _|"," _|"];
+let four = ["   ", "|_|", "  |"];
+let five = [" _ ", "|_ ", " _|"];
+let six = [" _ ", "|_ ", "|_|"];
+let seven = [" _ ", "  |", "  |"];
+let eight = [" _ ", "|_|", "|_|"];
+let nine = [" _ ", "|_|", "  |"];
+let digits = [zero, one, two, three, four, five, six, seven, eight, nine];
 
 function printDoubleDigit(value){
+    
     switch(value){
-        case (value = 0): function printSingles(){
+        case (value == 0): 
+            function printSingles(){
 
-        }
+            }
+            break;
         
-        case (value = 1):function printTens(){
+        case (value == 1):
+            function printTens(){
         
-        }
+            }
+            break;
+
+        case (value == 2):
+            function printTwinties(){
         
-        case (value = 2):function printTwinties(){
+            }
+            break;
         
-        }
+        case (value == 3):
+            function printThirties(){
         
-        case (value = 3):function printThirties(){
+            }
+            break;
+
         
-        }
+        case (value == 4):
+            function printFourties(){
         
-        case (value = 4):function printFourties(){
+            }
+            break;
         
-        }
+        case (value == 5):
+            function printFifties(){
         
-        case (value = 5):function printFifties(){
+            }
+            break;
         
-        }
+        case (value == 6):
+            function printSixties(){
+                for(f=0;f<digits.length;f++)
+                {
+                    
+                    for (i = 0; i < six.length; i++)
+                    {
+                        console.log(six[i] + " " + digits[f][i])
+                    }
+                }
+                printSixties();
+    
+            }
+            break;
         
-        case (value = 6):function printSixties(){
+        case (value == 7):
+            function printSeventies(){
+                for(f=0;f<digits.length;f++)
+                {
+                    
+                    for (i = 0; i < seven.length; i++)
+                    {
+                        console.log(seven[i] + " " + digits[f][i]);
+                    }
+                }
+            
+                printSeventies();
+            }
+        break;
         
-        }
+        case (value == 8):
+            
+            function printEighties(){
+                let eighties = "";
+                let firstDigit = digits[8];
+                for (i=0; i < digits.length; i++){
+                    
+                    let secondDigit = digits[i];
+                    
+                    for (j = 0; j < 3; j++){
+                        eighties += firstDigit[j] + secondDigit[j];
+                        eighties += "\n"; 
+                    }
+                }     
+                console.log(eighties);
+            }
+            break;
         
-        case (value = 7):function printSeventies(){
-        
-        }
-        
-        case (value = 8):function printEighties(){
-        
-        }
-        
-        case (value = 9):function printNinties(){
-        
-        }
+        case (value == 9):
+            function printNinties(){
+                let nineties = "";
+                let firstDigit = digits[9];
+                for (i=0; i < digits.length; i++){
+                    
+                    let secondDigit = digits[i];
+                    
+                    for (j = 0; j < 3; j++){
+                        nineties += firstDigit[j] + secondDigit[j];
+                        nineties += "\n"; 
+                    }
+                }     
+                console.log(nineties);
+                    
+            }
+            
     }    
 }
+
+
+printDoubleDigit(8);
+printDoubleDigit(9);
